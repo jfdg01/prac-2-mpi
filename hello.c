c965501dd35da554cbfcfaa1510db20db85dd954
Javier Dibo <javier.fco.dibo.gomez@gmail.com>
2026-05-04 18:28:52 +0200

    Hecho

---
+++ hello.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <mpi.h>
+
+int main(int argc, char* argv[]) {
+
+    MPI_Init(&argc, &argv);
+
+    int num;
+    MPI_Comm_size(MPI_COMM_WORLD, &num);
+
+    if (num % 2 != 0) {
+        printf("Se necesita un número par de procesos.\n");
+        MPI_Finalize();
+        return 0;
+    }
+
+    int rango;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rango);
+
+    int es_par;
+    if (rango % 2 == 0)
+        es_par = 1;
+    else
+        es_par = 0;
+    int mi_pareja;
+
+    if (es_par)
+        mi_pareja = rango+1;
+    else
+        mi_pareja = rango-1;
+
+    int valor = 0;
+    int contador = 0;
+
+    while (contador < 3) {
+        if (es_par) {
+            if (contador == 0)
+                printf("Soy el proceso %d y juego con el proceso %d. Tengo  %d y envío %d\n", rango, mi_pareja, valor, valor+1);
+            else
+                printf("Soy el proceso %d y juego con el proceso %d. Recibo %d y envío %d\n", rango, mi_pareja, valor, valor+1);
+            valor++;
+            MPI_Send(&valor, 1, MPI_INT, mi_pareja, 0, MPI_COMM_WORLD);
+            MPI_Recv(&valor, 1, MPI_INT, mi_pareja, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        }
+        else {
+            MPI_Recv(&valor, 1, MPI_INT, mi_pareja, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+            printf("Soy el proceso %d y juego con el proceso %d. Recibo %d y envío %d\n", rango, mi_pareja, valor, valor + 1);
+            valor++;
+            MPI_Send(&valor, 1, MPI_INT, mi_pareja, 0, MPI_COMM_WORLD);
+        }
+        contador++;
+    }
+
+    MPI_Finalize();
+    return 0;
+}
